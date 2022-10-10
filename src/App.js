79b6361204a8eac00a158d3f00bb7ad76859de14
minsoo <minsu2344@naver.com>
2022-10-10 16:00:42 +0900

    Link NavLink useParams useSearchParams useNavigate 등

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Layout from './Layout'
+import About from './pages/About'
+import Article from './pages/Article'
+import Articles from './pages/Articles'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Routes>
+      {/* Layout을 element만 이용해서 넣기 */}
+      <Route element={<Layout />}>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/profiles/:username' element={<Profile />} />
+      </Route>
+      {/* 아래처럼 사용도 가능 */}
+      <Route path='/' element={<Layout />}>
+        {/* index는 상위 path와 동일한 path값 가짐 */}
+        <Route index element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/profiles/:username' element={<Profile />} />
+      </Route>
+
+      <Route path='articles' element={<Articles/>}>
+        <Route path=':id' element={<Article />} />
+      </Route>
+    </Routes>
+  )
 }
 
-export default App;
+export default App
