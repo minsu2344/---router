22f2d2a2c76bfeed93deb7e6e6fdcd11a3badffc
minsoo <minsu2344@naver.com>
2022-10-10 20:04:39 +0900

    Navigate 이용하여 로그인 여부 확인하여 페이지 이동

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import About from './pages/About'
 import Article from './pages/Article'
 import Articles from './pages/Articles'
 import Home from './pages/Home'
+import Login from './pages/Login'
+import MyPage from './pages/MyPage'
 import NotFound from './pages/NotFound'
 import Profile from './pages/Profile'
 
@@ -20,8 +22,9 @@ function App() {
       <Route path='articles' element={<Articles/>}>
         <Route path=':id' element={<Article />} />
       </Route>
-      {/* '*'는 wildcard 문자 */}
-      {/* 상단에 위치한 모든 라우트들과 매칭되지 않으면 아래 라우트 실행 */}
+      <Route path='/login' element={<Login />} />
+      <Route path='/mypage' element={<MyPage />} />
+
       <Route path='*' element={<NotFound />} />
     </Routes>
   )
